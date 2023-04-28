d45c42f9e198827e941f1af60a00019a1a66960b
Muhammed-Saleh <hamada500005@gmail.com>
2023-04-27 21:26:01 -0700

    Adding js code to change the icons sequently

---
+++ script.js
@@ -0,0 +1,20 @@
+const icons = document.querySelectorAll(
+  ".section01__icons-wrapper .section01__icon"
+);
+
+let i = 1;
+
+setInterval(() => {
+  i++;
+  const icon = document.querySelector(
+    ".section01__icons-wrapper .section01__icon--change"
+  );
+  icon.classList.remove("section01__icon--change");
+
+  if (i > icons.length) {
+    icons[0].classList.add("section01__icon--change");
+    i = 1;
+  } else {
+    icon.nextElementSibling.classList.add("section01__icon--change");
+  }
+}, 1000);
